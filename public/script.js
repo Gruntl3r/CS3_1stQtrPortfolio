25784ecba4137c5fe38cbd4861a363a38187f224
Gruntl3r <b2029hctdumlao@pshs.edu.ph>
2025-08-05 08:43:45 +0800

    First Commit

---
+++ public/script.js
@@ -0,0 +1,7 @@
+let age = 13;
+let maxAge = 100;
+let numPerDay = 20;
+
+let totalRequired = (numPerDay * 365) * (maxAge - age);
+
+document.write("You will need " + totalRequired + " to last until you ripe old age of " + maxAge);
