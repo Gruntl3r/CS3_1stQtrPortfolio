060ab2a14a2193674209600139962529e73ba83d
Gruntl3r <b2029hctdumlao@pshs.edu.ph>
2025-08-12 08:51:34 +0800

    First Commit

--- public/script.js
+++ public/script.js
@@ -2,27 +2,27 @@ let age;
 let maxAge;
 let numPerDay;
 
-let unc = prompt("What is your age? ", "age");
-let max = prompt("WHat is your maximum age? ", "maxAge");
-let num = prompt("How many times do you eat your snack per day? ", "numPerDay");
-if (confirm('Is 'unc' really your age?')){
+//let unc = prompt("What is your age? ", "age");
+//let max = prompt("WHat is your maximum age? ", "maxAge");
+//let num = prompt("How many times do you eat your snack per day? ", "numPerDay");
+//if (confirm('Is 'unc' really your age?')){
     console.log("unc");
-}
-else{
+//}
+//else{
     console.log("liar!");
-}
-if (confirm('Is 'max' really your age?')){
+//}
+//if (confirm('Is 'max' really your age?')){
     console.log("healthy");
-}
-else{
+//}
+//else{
     console.log("eat veggies");
-}
-if (confirm('Is 'num' really your age?')){
+//}
+//if (confirm('Is 'num' really your age?')){
     console.log("calm down!");
-}
-else{
+//}
+//else{
     console.log("I knew it!");
-}
+//}
 let totalRequired = (num * 365) * (max - unc);
 
 document.write("You will need " + totalRequired + " to last until you ripe old age of " + max);
